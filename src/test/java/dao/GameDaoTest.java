889d805783f6672ab693a09ad62025fe5287b1e2
julien.girardot <girardot.jul@gmail.com>
2013-08-13 22:27:56 +0200

    test dao

--- src/test/java/dao/GameDaoTest.java
+++ src/test/java/dao/GameDaoTest.java
@@ -1,20 +1,39 @@
 package dao;
 
-import org.junit.Ignore;
+import com.sun.xml.internal.ws.policy.AssertionSet;
+import model.Game;
+import org.fest.assertions.Assertions;
+import org.junit.Before;
 import org.junit.Test;
+
+
+import java.io.FileNotFoundException;
+import java.util.List;
 
 public class GameDaoTest {
 
     public GameDao gameDao;
 
-    public GameDaoTest(GameDao gameDao) {
-        this.gameDao = gameDao;
+    public GameDaoTest() {
     }
 
-//    @Ignore
-//    @Test
-//    public void should_() {
-//
-//    }
+    @Before
+    public void setUp() throws Exception {
+        gameDao = new GameDao();
+    }
+
+    @Test
+    public void should_() {
+        // When
+        List<Game> games = null;
+        try {
+            games = gameDao.findGames();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+
+        // Then
+        Assertions.assertThat(games.size()).isEqualTo(3);
+    }
 
 }
